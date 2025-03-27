2017f3ce1aee60d49f9628c9a264210c02411048
rahul9798540 <rahul979854@gmail.com>
2025-03-28 00:44:06 +0530

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+  const url = `https://linearjitp-playback.astro.com.my/dash-wv/linear/2504/default_ott.mpd`;
+  
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "X-Forwarded-For": "103.3.64.1",  // Fake SG IP
+        "User-Agent": "Mozilla/5.0 (Linux; Android 10; SM-G977N)",
+        "Referer": "https://astro.com.my/"
+      }
+    });
+
+    if (!response.ok) {
+      return res.status(404).send("Not Found");
+    }
+
+    const data = await response.text();
+    res.setHeader("Content-Type", "application/dash+xml");
+    res.send(data);
+  } catch (error) {
+    res.status(500).send("Error fetching MPD file");
+  }
+}
